82f94742a78cadaa3f44468218241eb5334b1088
nbt <nezihetekin@nezihes-mbp.home>
2024-09-15 12:20:26 -0400

    add local folder to repo

---
+++ Finished/Overview/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+
+            string str = Console.ReadLine();
+            Console.WriteLine("Why, hello there " + str);
+        }
+    }
+}
